be51725c690c504f10be7da64b81378e26e11315
Ivan Marban <ivan.marban@gmail.com>
2022-07-07 21:11:46 +0200

    feat: backup service

---
+++ src/main/java/com/github/ivanmarban/backup/BackupService.java
@@ -0,0 +1,52 @@
+package com.github.ivanmarban.backup;
+
+import com.github.ivanmarban.gdrive.GoogleDriveService;
+import com.google.api.services.drive.model.File;
+import jakarta.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+@Singleton
+@Slf4j
+public class BackupService {
+
+    private static final List<String> FILES = Arrays.asList(OpenHabBackup.TAR_GZ_FILENAME,
+            TasmotaBackup.TAR_GZ_FILENAME);
+
+    private final TasmotaBackup tasmotaBackup;
+
+    private final OpenHabBackup openHabBackup;
+
+    private final GoogleDriveService googleDriveService;
+
+    public BackupService(TasmotaBackup tasmotaBackup, OpenHabBackup openHabBackup,
+            GoogleDriveService googleDriveService) {
+        this.tasmotaBackup = tasmotaBackup;
+        this.openHabBackup = openHabBackup;
+        this.googleDriveService = googleDriveService;
+    }
+
+    public void createBackup() throws IOException {
+        Path tempFolder = Files.createTempDirectory("iot-backup-manager");
+        tasmotaBackup.create(tempFolder);
+        openHabBackup.create(tempFolder);
+        FILES.forEach(file -> uploadBackupFile(tempFolder, file));
+    }
+
+    private void uploadBackupFile(Path tempFolder, String fileName) {
+        googleDriveService.getFileId(fileName).ifPresentOrElse(fileId -> {
+            File file = googleDriveService.updateFile(fileId, tempFolder.resolve(fileName));
+            log.info("File updated successfully [id={}, name={}, version={}]", file.getId(), file.getName(),
+                    file.getVersion());
+        }, () -> {
+            File file = googleDriveService.uploadFile(tempFolder.resolve(fileName));
+            log.info("File uploaded successfully [id={}, name={}]", file.getId(), file.getName());
+        });
+    }
+
+}
---
+++ src/test/java/com/github/ivanmarban/backup/BackupServiceTest.java
@@ -0,0 +1,67 @@
+package com.github.ivanmarban.backup;
+
+import com.github.ivanmarban.gdrive.GoogleDriveService;
+import com.google.api.services.drive.model.File;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BackupServiceTest {
+
+    private static final String FILE_NAME = "file.txt";
+
+    private static final String FILE_ID = "1uG6PqmklsDiKDnwvsobC80KyqbDpxByu";
+
+    private static final long FILE_VERSION = 1;
+
+    @Mock
+    private TasmotaBackup tasmotaBackup;
+
+    @Mock
+    private OpenHabBackup openHabBackup;
+
+    @Mock
+    private GoogleDriveService googleDriveService;
+
+    @InjectMocks
+    private BackupService backupService;
+
+    @Test
+    @DisplayName("Should create a new file in Google Drive")
+    public void testBackupServiceShouldUploadFiles() throws IOException {
+        when(googleDriveService.uploadFile(any(Path.class))).thenReturn(mockFile());
+        backupService.createBackup();
+        verify(tasmotaBackup, times(1)).create(any(Path.class));
+        verify(openHabBackup, times(1)).create(any(Path.class));
+        verify(googleDriveService, times(2)).getFileId(anyString());
+        verify(googleDriveService, times(2)).uploadFile(any(Path.class));
+    }
+
+    @Test
+    @DisplayName("Should update a file's metadata and/or content in Google Drive")
+    public void testBackupServiceShouldUpdateFiles() throws IOException {
+        when(googleDriveService.updateFile(anyString(), any(Path.class))).thenReturn(mockFile());
+        when(googleDriveService.getFileId(anyString())).thenReturn(Optional.of("foo"));
+        backupService.createBackup();
+        verify(tasmotaBackup, times(1)).create(any(Path.class));
+        verify(openHabBackup, times(1)).create(any(Path.class));
+        verify(googleDriveService, times(2)).getFileId(anyString());
+        verify(googleDriveService, times(0)).uploadFile(any(Path.class));
+        verify(googleDriveService, times(2)).updateFile(anyString(), any(Path.class));
+    }
+
+    private File mockFile() {
+        return new File().setName(FILE_NAME).setId(FILE_ID).setVersion(FILE_VERSION);
+    }
+
+}
